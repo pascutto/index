0e27d0e71577e6e166d7eedb6107bf76d0ced249
Clément Pascutto <clement@tarides.com>
2020-01-13 15:24:10 +0100

    Try first class modules

--- bench/bench.ml
+++ bench/bench.ml
@@ -1,29 +1,3 @@
-let reporter ?(prefix = "") () =
-  let report src level ~over k msgf =
-    let k _ =
-      over ();
-      k ()
-    in
-    let with_stamp h _tags k fmt =
-      match level with
-      | Logs.App -> Fmt.kpf k Fmt.stdout (fmt ^^ "@.%!")
-      | _ ->
-          let ppf = Fmt.stderr in
-          let dt = Unix.gettimeofday () in
-          Fmt.kpf k ppf
-            ("%s%+04.0fus %a %a @[" ^^ fmt ^^ "@]@.")
-            prefix dt
-            Fmt.(styled `Magenta string)
-            (Logs.Src.name src) Logs_fmt.pp_header (level, h)
-    in
-    msgf @@ fun ?header ?tags fmt -> with_stamp header tags k fmt
-  in
-  { Logs.report }
-
-let src = Logs.Src.create "db_bench"
-
-module Log = (val Logs.src_log src : Logs.LOG)
-
 module Stats = Index.Stats
 
 let src =
@@ -41,59 +15,34 @@ let src =
   in
   Src.v "bench" ~tags ~data
 
-let key_size = 32
-
-let hash_size = 30
-
-let value_size = 13
-
-let entry_size = key_size + value_size
-
-let nb_entries = 1_000_000
-
-let log_size = 500_000
+let add_metrics =
+  let no_tags x = x in
+  fun () -> Metrics.add src no_tags (fun m -> m (Stats.get ()))
 
 let ( // ) = Filename.concat
 
-let random_char () = char_of_int (33 + Random.int 94)
+module Config = struct
+  type t = {
+    key_size : int;
+    value_size : int;
+    with_metrics : bool;
+    seed : int;
+    data_dir : string;
+    nb_entries : int;
+    log_size : int;
+  }
+  [@@deriving yojson]
 
-let random_string string_size =
-  String.init string_size (fun _i -> random_char ())
+  let _pp fmt t =
+    Format.fprintf fmt
+      "Size of keys: %d bytes.@\n\
+       Size of values: %d bytes.@\n\
+       Number of bindings: %d.@n@\n\
+       Log size: %d.@\n\
+       Initializing data with seed %d." t.key_size t.value_size t.nb_entries
+      t.log_size t.seed
 
-module Context = struct
-  module Key = struct
-    type t = string
-
-    let v () = random_string key_size
-
-    let hash = Hashtbl.hash
-
-    let hash_size = hash_size
-
-    let encode s = s
-
-    let decode s off = String.sub s off key_size
-
-    let encoded_size = key_size
-
-    let equal = String.equal
-
-    let pp s = Fmt.fmt "%s" s
-  end
-
-  module Value = struct
-    type t = string
-
-    let v () = random_string value_size
-
-    let encode s = s
-
-    let decode s off = String.sub s off value_size
-
-    let encoded_size = value_size
-
-    let pp s = Fmt.fmt "%s" s
-  end
+  let _pp_json fmt t = Yojson.Safe.pretty_print fmt (to_yojson t)
 end
 
 let with_stats f =
@@ -116,10 +65,10 @@ module Benchmark = struct
   }
   [@@deriving yojson]
 
-  let run ~with_metrics f =
-    let _, time, stats = with_stats (fun () -> f ~with_metrics ()) in
-    let nb_entriesf = Float.of_int nb_entries in
-    let entry_sizef = Float.of_int entry_size in
+  let run ~(config : Config.t) (f : config:Config.t -> unit -> unit) =
+    let _, time, stats = with_stats (fun () -> f ~config ()) in
+    let nb_entriesf = Float.of_int config.nb_entries in
+    let entry_sizef = Float.of_int (config.key_size + config.value_size) in
     let read_amplification_size =
       Float.of_int stats.bytes_read /. (entry_sizef *. nb_entriesf)
     in
@@ -159,114 +108,172 @@ module Benchmark = struct
     Yojson.Safe.pretty_print fmt (result_to_yojson result)
 end
 
-let make_bindings_pool () =
-  Array.init nb_entries (fun _ ->
-      let k = Context.Key.v () in
-      let v = Context.Value.v () in
-      (k, v))
+module type Key = sig
+  include Index.Key
 
-let bindings_pool = ref [||]
+  val v : unit -> t
 
-let absent_bindings = ref [||]
+  val compare : t -> t -> int
+end
 
-module Index = struct
-  module Index = Index_unix.Private.Make (Context.Key) (Context.Value)
+module type Value = sig
+  include Index.Value
 
-  type index_v = fresh:bool -> readonly:bool -> string -> Index.t
+  val v : unit -> t
 
-  type benchmark = with_metrics:bool -> index_v -> unit -> unit
+  val compare : t -> t -> int
+end
 
-  let add_metrics =
-    let no_tags x = x in
-    fun () -> Metrics.add src no_tags (fun m -> m (Stats.get ()))
+module type Bench = sig
+  module Index : Index.S
 
-  let write ~with_metrics ?(with_flush = false) bindings rw =
+  type key
+
+  type value
+
+  val make_bindings_pool : config:Config.t -> (key * value) array
+
+  val bindings_pool : (key * value) array ref
+
+  val absent_bindings_pool : (key * value) array ref
+
+  val run :
+    config:Config.t ->
+    root:string ->
+    name:string ->
+    (config:Config.t ->
+    (fresh:bool -> readonly:bool -> string -> Index.t) ->
+    unit ->
+    unit) ->
+    Benchmark.result
+
+  type suite_elt = {
+    name : string;
+    synopsis : string;
+    benchmark :
+      config:Config.t ->
+      (fresh:bool -> readonly:bool -> string -> Index.t) ->
+      unit ->
+      unit;
+    dependency : string option;
+  }
+
+  val suite : suite_elt list
+
+  val pp_suite : Format.formatter -> suite_elt list -> unit
+
+  val schedule : (string -> bool) -> suite_elt list -> suite_elt list
+end
+
+module Index_bench (Key : Key) (Value : Value) : Bench = struct
+  module Index = Index_unix.Make (Key) (Value)
+
+  type key = Key.t
+
+  type value = Value.t
+
+  let make_bindings_pool ~(config : Config.t) =
+    Array.init config.nb_entries (fun _ ->
+        let k = Key.v () in
+        let v = Value.v () in
+        (k, v))
+
+  let bindings_pool = ref [||]
+
+  let absent_bindings_pool = ref [||]
+
+  let write ~(config : Config.t) ?(with_flush = false) bindings rw =
     Array.iter
       (fun (k, v) ->
         Index.replace rw k v;
         if with_flush then Index.flush rw;
-        if with_metrics then add_metrics ())
+        if config.with_metrics then add_metrics ())
       bindings
 
-  let read ~with_metrics bindings r =
+  let read ~(config : Config.t) bindings r =
     Array.iter
-      (fun (k, (_ : Context.Value.t)) ->
-        ignore (Index.find r k : Context.Value.t);
-        if with_metrics then add_metrics ())
+      (fun (k, _) ->
+        ignore (Index.find r k);
+        if config.with_metrics then add_metrics ())
       bindings
 
-  let read_absent ~with_metrics bindings r =
+  let read_absent ~(config : Config.t) bindings r =
     Array.iter
       (fun (k, _) ->
         try
-          ignore (Index.find r k : Context.Value.t);
+          ignore (Index.find r k);
           failwith "Absent value found"
-        with Not_found -> if with_metrics then add_metrics ())
+        with Not_found -> if config.with_metrics then add_metrics ())
       bindings
 
-  let write_random ~with_metrics v () =
-    v ~fresh:true ~readonly:false "" |> write ~with_metrics !bindings_pool
+  let write_random ~config v () =
+    v ~fresh:true ~readonly:false "" |> write ~config !bindings_pool
 
-  let write_seq ~with_metrics v =
+  let write_seq ~config v =
     let bindings = Array.copy !bindings_pool in
-    Array.sort (fun a b -> String.compare (fst a) (fst b)) bindings;
-    fun () -> v ~fresh:true ~readonly:false "" |> write ~with_metrics bindings
+    Array.sort (fun a b -> Key.compare (fst a) (fst b)) bindings;
+    fun () -> v ~fresh:true ~readonly:false "" |> write ~config bindings
 
-  let write_seq_hash ~with_metrics v =
-    let hash e = Context.Key.hash (fst e) in
+  let write_seq_hash ~config v =
+    let hash e = Hashtbl.hash (fst e) in
     let bindings = Array.copy !bindings_pool in
     Array.sort (fun a b -> Int.compare (hash a) (hash b)) bindings;
-    fun () -> v ~fresh:true ~readonly:false "" |> write ~with_metrics bindings
+    fun () -> v ~fresh:true ~readonly:false "" |> write ~config bindings
 
-  let write_rev_seq_hash ~with_metrics v =
-    let hash e = Context.Key.hash (fst e) in
+  let write_rev_seq_hash ~config v =
+    let hash e = Hashtbl.hash (fst e) in
     let bindings = Array.copy !bindings_pool in
     Array.sort (fun a b -> -Int.compare (hash a) (hash b)) bindings;
-    fun () -> v ~fresh:true ~readonly:false "" |> write ~with_metrics bindings
+    fun () -> v ~fresh:true ~readonly:false "" |> write ~config bindings
 
-  let write_sync ~with_metrics v () =
+  let write_sync ~config v () =
     v ~fresh:true ~readonly:false ""
-    |> write ~with_metrics ~with_flush:true !bindings_pool
+    |> write ~config ~with_flush:true !bindings_pool
 
-  let iter ~with_metrics v () =
+  let iter ~(config : Config.t) v () =
     v ~fresh:false ~readonly:true ""
-    |> Index.iter (fun _ _ -> if with_metrics then add_metrics ())
+    |> Index.iter (fun _ _ -> if config.with_metrics then add_metrics ())
 
-  let find_random ~with_metrics v () =
-    v ~fresh:false ~readonly:false "" |> read ~with_metrics !bindings_pool
+  let find_random ~config v () =
+    v ~fresh:false ~readonly:false "" |> read ~config !bindings_pool
 
-  let find_random_ro ~with_metrics v () =
-    v ~fresh:false ~readonly:true "" |> read ~with_metrics !bindings_pool
+  let find_random_ro ~config v () =
+    v ~fresh:false ~readonly:true "" |> read ~config !bindings_pool
 
-  let find_absent ~with_metrics v () =
+  let find_absent ~config v () =
     v ~fresh:false ~readonly:false ""
-    |> read_absent ~with_metrics !absent_bindings
+    |> read_absent ~config !absent_bindings_pool
 
-  let find_absent_ro ~with_metrics v () =
+  let find_absent_ro ~config v () =
     v ~fresh:false ~readonly:true ""
-    |> read_absent ~with_metrics !absent_bindings
+    |> read_absent ~config !absent_bindings_pool
 
-  let run :
-      with_metrics:bool ->
-      root:string ->
-      name:string ->
-      benchmark ->
-      Benchmark.result =
-   fun ~with_metrics ~root ~name b ->
+  let run ~(config : Config.t) ~root ~name
+      (b :
+        config:Config.t ->
+        (fresh:bool -> readonly:bool -> string -> Index.t) ->
+        unit ->
+        unit) =
     let indices = ref [] in
     let index_v ~fresh ~readonly n =
-      let i = Index.v ~fresh ~readonly ~log_size (root // name // n) in
-      indices := i :: !indices;
-      i
+      let v =
+        Index.v ~fresh ~readonly ~log_size:config.log_size (root // name // n)
+      in
+      indices := v :: !indices;
+      v
     in
-    let result = Benchmark.run ~with_metrics (b index_v) in
+    let result = Benchmark.run ~config (b index_v) in
     !indices |> List.iter (fun i -> Index.close i);
     result
 
   type suite_elt = {
     name : string;
     synopsis : string;
-    benchmark : benchmark;
+    benchmark :
+      config:Config.t ->
+      (fresh:bool -> readonly:bool -> string -> Index.t) ->
+      unit ->
+      unit;
     dependency : string option;
   }
 
@@ -333,61 +340,116 @@ module Index = struct
         dependency = Some "replace_random";
       };
     ]
+
+  let pp_suite fmt suite =
+    let pp_bench ppf b = Fmt.pf ppf "%s\t-- %s" b.name b.synopsis in
+    Format.fprintf fmt "%a" Fmt.(list ~sep:(const string "\n") pp_bench) suite
+
+  let schedule p s =
+    let todos = List.map fst in
+    let init = ref (s |> List.map (fun b -> (p b.name, b))) in
+    let apply_dep s =
+      let deps =
+        s
+        |> List.fold_left
+             (fun acc (todo, b) ->
+               if todo then
+                 match b.dependency with Some s -> s :: acc | None -> acc
+               else acc)
+             []
+      in
+      s |> List.map (fun (todo, b) -> (todo || List.mem b.name deps, b))
+    in
+    let next = ref (apply_dep !init) in
+    while todos !init <> todos !next do
+      init := !next;
+      next := apply_dep !init
+    done;
+    let r = List.filter fst !init |> List.map snd in
+    r
 end
 
-let list_benchs () =
-  let pp_bench ppf b = Fmt.pf ppf "%s\t-- %s" b.Index.name b.synopsis in
-  Index.suite |> Fmt.(pr "%a" (list ~sep:Fmt.(const string "\n") pp_bench))
+let _list_benchs (module Bench : Bench) () =
+  Format.printf "%a" Bench.pp_suite Bench.suite
 
-let schedule p s =
-  let todos = List.map fst in
-  let init = ref (s |> List.map (fun b -> (p b.Index.name, b))) in
-  let apply_dep s =
-    let deps =
-      s
-      |> List.fold_left
-           (fun acc (todo, b) ->
-             if todo then
-               match b.Index.dependency with Some s -> s :: acc | None -> acc
-             else acc)
-           []
-    in
-    s |> List.map (fun (todo, b) -> (todo || List.mem b.Index.name deps, b))
+let random_string string_size =
+  let random_char () = char_of_int (33 + Random.int 94) in
+  String.init string_size (fun _i -> random_char ())
+
+let make_bench (config : Config.t) =
+  let key =
+    ( module struct
+      type t = string
+
+      let v () = random_string config.key_size
+
+      let compare = String.compare
+
+      let hash = Hashtbl.hash
+
+      let hash_size = 30
+
+      let encode s = s
+
+      let decode s off = String.sub s off config.key_size
+
+      let encoded_size = config.key_size
+
+      let equal = String.equal
+
+      let pp s = Fmt.fmt "%s" s
+    end : Key )
   in
-  let next = ref (apply_dep !init) in
-  while todos !init <> todos !next do
-    init := !next;
-    next := apply_dep !init
-  done;
-  let r = List.filter fst !init |> List.map snd in
-  r
+  let value =
+    ( module struct
+      type t = string
 
-let init with_metrics seed =
-  Logs.set_level (Some Logs.App);
-  Logs.set_reporter (reporter ());
-  Random.init seed;
-  if with_metrics then (
+      let v () = random_string config.value_size
+
+      let compare = String.compare
+
+      let encode s = s
+
+      let decode s off = String.sub s off config.value_size
+
+      let encoded_size = config.value_size
+
+      let pp s = Fmt.fmt "%s" s
+    end : Value )
+  in
+  (module Index_bench ((val key)) ((val value)) : Bench)
+
+let init (config : Config.t) =
+  Random.init config.seed;
+  if config.with_metrics then (
     Metrics.enable_all ();
     Metrics_gnuplot.set_reporter ();
     Metrics_unix.monitor_gc 0.1 );
-  Log.app (fun l -> l "Size of keys: %d bytes." key_size);
-  Log.app (fun l -> l "Size of values: %d bytes." value_size);
-  Log.app (fun l -> l "Number of bindings: %d." nb_entries);
-  Log.app (fun l -> l "Log size: %d." log_size);
-  Log.app (fun l -> l "Initializing data with seed %d.\n" seed);
-  bindings_pool := make_bindings_pool ();
-  absent_bindings := make_bindings_pool ()
+  config |> make_bench
 
-let run filter data_dir seed with_metrics =
-  init with_metrics seed;
+let run filter key_size value_size with_metrics seed data_dir nb_entries
+    log_size =
+  let config =
+    Config.
+      {
+        key_size;
+        value_size;
+        with_metrics;
+        seed;
+        data_dir;
+        nb_entries;
+        log_size;
+      }
+  in
   let name_filter =
     match filter with None -> fun _ -> true | Some re -> Re.execp re
   in
-  Index.suite
-  |> schedule name_filter
-  |> List.iter (fun Index.{ synopsis; name; benchmark; dependency } ->
+  let module Bench = (val init config) in
+  Bench.suite
+  |> Bench.schedule name_filter
+  |> List.iter (fun Bench.{ synopsis; name; benchmark; dependency } ->
          let name = match dependency with None -> name | Some name -> name in
-         let result = Index.run ~with_metrics ~root:data_dir ~name benchmark in
+         let result = Bench.run ~config ~root:data_dir ~name benchmark in
          Logs.app (fun l ->
              l "%s@\n    @[%a@]@\n" synopsis Benchmark.pp_result result))
 
@@ -413,23 +475,48 @@ let data_dir =
   let doc = "Set directory for the data files" in
   Arg.(value & opt dir "_bench" & info [ "d"; "directory" ] ~doc)
 
+let key_size =
+  let doc = "Sets the size of the keys." in
+  Arg.(value & opt int 32 & info [ "key_size" ] ~doc)
+
+let value_size =
+  let doc = "Sets the size of the values." in
+  Arg.(value & opt int 13 & info [ "value_size" ] ~doc)
+
 let seed =
   let doc = "The seed used to generate random data." in
   Arg.(value & opt int 0 & info [ "s"; "seed" ] ~doc)
 
-let metrics_flag =
+let with_metrics =
   let doc = "Use Metrics; note that it has an impact on performance" in
   Arg.(value & flag & info [ "m"; "with_metrics" ] ~doc)
 
-let list_cmd =
+let nb_entries =
+  let doc = "Sets the number of bindings." in
+  Arg.(value & opt int 10_000_000 & info [ "entries" ] ~doc)
+
+let log_size =
+  let doc = "Sets the log size." in
+  Arg.(value & opt int 500_000 & info [ "log_size" ] ~doc)
+
+(* let list_cmd =
   let doc = "List all available benchmarks." in
-  (Term.(pure list_benchs $ const ()), Term.info "list" ~doc)
+  (Term.(pure list_benchs $ const ()), Term.info "list" ~doc) *)
 
 let cmd =
   let doc = "Run all the benchmarks." in
-  ( Term.(const run $ name_filter $ data_dir $ seed $ metrics_flag),
+  ( Term.(
+      const run
+      $ name_filter
+      $ key_size
+      $ value_size
+      $ with_metrics
+      $ seed
+      $ data_dir
+      $ nb_entries
+      $ log_size),
     Term.info "run" ~doc ~exits:Term.default_exits )
 
 let () =
-  let choices = [ list_cmd ] in
+  let choices = [ (* list_cmd *) ] in
   Term.(exit @@ eval_choice cmd choices)
