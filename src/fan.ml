f720a354d61559a9bec48ce67803478e9c4f21bf
Clément Pascutto <clement@pascutto.fr>
2021-02-19 15:22:45 +0100

    Allocate less during Fan.{import,export}

--- src/fan.ml
+++ src/fan.ml
@@ -68,41 +68,29 @@ external get_64 : string -> int -> int64 = "%caml_string_get64"
 
 external swap64 : int64 -> int64 = "%bswap_int64"
 
-let encode_int64 i =
-  let set_uint64 s off v =
-    if not Sys.big_endian then set_64 s off (swap64 v) else set_64 s off v
-  in
-  let b = Bytes.create 8 in
-  set_uint64 b 0 i;
-  Bytes.to_string b
+let encode_int64 t buf ~off =
+  if not Sys.big_endian then set_64 buf off (swap64 t) else set_64 buf off t
 
-let decode_int64 buf =
-  let get_uint64 s off =
-    if not Sys.big_endian then swap64 (get_64 s off) else get_64 s off
-  in
-  get_uint64 buf 0
+let decode_int64 buf ~off =
+  if not Sys.big_endian then swap64 (get_64 buf off) else get_64 buf off
 
 let exported_size t = Array.length t.fans * 8
 
 let export t =
   let encoded_size = exported_size t in
-  let buf = Buffer.create encoded_size in
+  let buf = Bytes.create encoded_size in
   let rec loop i =
     if i >= Array.length t.fans then ()
     else (
-      Buffer.add_string buf (encode_int64 t.fans.(i));
+      encode_int64 t.fans.(i) buf ~off:(i * 8);
       loop (i + 1))
   in
   loop 0;
-  Buffer.contents buf
+  Bytes.unsafe_to_string buf
 
 let import ~hash_size buf =
   let nb_fans = String.length buf / 8 in
-  let fans =
-    Array.init nb_fans (fun i ->
-        let sub = String.sub buf (i * 8) 8 in
-        decode_int64 sub)
-  in
+  let fans = Array.init nb_fans (fun i -> decode_int64 buf ~off:(i * 8)) in
   let size = int_of_float (log2 (float_of_int nb_fans)) in
   let shift = hash_size - size in
   let mask = (nb_fans - 1) lsl shift in
